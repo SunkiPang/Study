2de7eee388ea22fb846664bce6054d5dc7f4a196
SunkiPang <49421213+SunkiPang@users.noreply.github.com>
2019-05-09 11:30:05 +0900

    Update simple.java

--- JAVA/11-2_recursion/simple.java
+++ JAVA/11-2_recursion/simple.java
@@ -2,12 +2,10 @@ class Main {
   public static void main(String[] args) {
     methodB(3);
   }
-  public static void methodB(int n){
-	if (n < 1)
-	  	System.out.println('B');
-	else{
-		methodB(n - 1);
-		System.out.println('R');
-	}
+  public static int Fibonacci(int n){
+    if (n == 1)
+      return 1;
   }
+   else
+      return (n + Fibonacci(n-1));
 }
