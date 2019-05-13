3d07cfa0a3f1dc6d49b24c974584620dfc950128
SunkiPang <49421213+SunkiPang@users.noreply.github.com>
2019-05-13 10:11:33 +0900

    Create HashSetDemo.java

---
+++ JAVA/12-1 array list/HashSetDemo.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+public class Main {
+	public static void main(String[] args) {
+			HashSet<Integer> intSet = new HashSet<Integer>(); intSet.add(2);
+			intSet.add(7);
+			intSet.add(7);// Ignored since 7 is already in the set intSet.add(3);
+			printSet(intSet); // method defined in the HashSetDemo class
+			intSet.remove(3);
+			printSet(intSet);// method defined in the HashSetDemo class
+			System.out.println("Set contains 2: " + intSet.contains(2)); System.out.println("Set contains 3: " + intSet.contains(3));
+		}
+
+	public static void printSet(HashSet<Integer> intSet){
+		System.out.println("The set contains:");
+		for (Object obj : intSet.toArray()){
+			Integer num = (Integer) obj; System.out.println(num.intValue());
+		}
+	}
+}
